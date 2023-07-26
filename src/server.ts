ed0738bd914dc8b1026ba6c5f6b04b28d70feef6
ludahz <ludahmaxs@gmail.com>
2023-07-26 16:00:31 +0500

    set up shop view and route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
+import shopRoutes from './routes/shop'
 
 const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use(shopRoutes)
 
 app.listen(3000, () => {
     console.log('App is listening at port 3000')
